585996701e720bfd0f1830a47dce19729b59b066
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-03-30 17:08:03 +0200

    herosection changes

--- web/src/components/homePage/HeroSection.js
+++ web/src/components/homePage/HeroSection.js
@@ -12,22 +12,14 @@ function HeroSection() {
       <div className="container">
         <div className="hero__wrapper">
           <div className="left">
-            <h1 className="hero__heading">DEMT</h1>
+            <h1 className="hero__heading">
+              Empowering rural communities to sustainability through
+              grassroots-driven environmentally friendly intervention methods
+            </h1>
             <ParagraphText className="hero__text">
-              Welcome to Disaster and Environmental Management Trust (D.E.M.T.),
-              your trusted partner in community-driven development. At D.E.M.T.,
-              we believe in the power of grassroots initiatives to drive lasting
-              change. Founded in 2019, our organization is dedicated to
-              fostering resilience and sustainable development starting from the
-              Mashonaland East province of Zimbabwe, propagating outwards. We
-              work hand-in-hand with local communities, implementing impactful
-              programs in water and sanitation, climate-smart agriculture, and
-              infrastructure rehabilitation. Through strategic partnerships with
-              local authorities, we empower communities to plan, mitigate, and
-              respond to environmental and climate-related challenges. Join us
-              in envisioning a future where rural communities thrive, equipped
-              with the knowledge and resources to overcome adversity. Together,
-              we're building a resilient tomorrow, one community at a time.
+              We actively leverage cutting edge solutions & collective effort to
+              compliment and augment vulnerable rural communities' ability to
+              envision and build a resilient tomorrow, one community at a time
             </ParagraphText>
             <SectionTitle>Our Vision</SectionTitle>
             <ParagraphText className="hero__text">
