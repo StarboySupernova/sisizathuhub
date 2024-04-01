10f27897cd995b4e6cc8f9b1b72b01b6c7112ec7
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2024-04-01 13:47:55 +0200

    color edits

--- web/src/styles/GlobalStyles.js
+++ web/src/styles/GlobalStyles.js
@@ -7,8 +7,8 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
   :root{
-    --primary: #4A46EB;
-    --secondary: #753AFF;
+    --primary: #052F11;
+    --secondary: #12933F;
     --darkBlue: #070747; 
     --darkPurple: #0E0034;
     --black-1: #0D121F;
