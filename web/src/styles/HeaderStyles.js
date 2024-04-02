7bd959819723bbf0d10b6d780689b68583f2f721
Christian Simbarashe Dombodzvuku <kristianosimba2108@gmail.com>
2024-04-02 03:32:10 +0200

    nav wrapper opacity

--- web/src/styles/HeaderStyles.js
+++ web/src/styles/HeaderStyles.js
@@ -72,7 +72,7 @@ export default styled.header`
         right: 0;
         width: 80%;
         transform: translateX(100%);
-        background: linear-gradient(135deg, #220b22, #08011c);
+        background-color: rgba(34, 11, 34, 0.5);
         height: 100vh;
         display: flex;
         align-items: center;
