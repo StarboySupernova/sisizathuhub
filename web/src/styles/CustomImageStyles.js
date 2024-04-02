1dac90e1247f39007e1239cb39d2e61c5bf47162
Simbarashe Dombodzvuku <supernovaonline@outlook.com>
2024-04-02 10:44:16 +0200

    custom image dimensions switched

--- web/src/styles/CustomImageStyles.js
+++ web/src/styles/CustomImageStyles.js
@@ -2,7 +2,9 @@ import styled from 'styled-components';
 
 export const CustomImageStyles = styled.div`
   .custom-image {
-    max-width: 200px;
-    max-height: 200px;
+    max-width: 180px;
+    max-height: 180px;
+    border-radius: 10px;
+    margin: 20px;
   }
 `;
